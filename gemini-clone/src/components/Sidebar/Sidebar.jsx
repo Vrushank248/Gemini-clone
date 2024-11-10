5b396697c08208635537d29f2166b920b5264c86
Vrushank248 <vrushankvyas248@gmail.com>
2024-11-10 15:07:12 +0530

    add top elements of sidebar

--- gemini-clone/src/components/Sidebar/Sidebar.jsx
+++ gemini-clone/src/components/Sidebar/Sidebar.jsx
@@ -5,7 +5,19 @@ const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className="top">
-            <img src={assets.menu_icon} alt="" />
+            <img className='menu' src={assets.menu_icon} alt="" />
+            <div className="new-chat">
+              <img src={assets.plus_icon} alt="" />
+              <p>New Chat</p>
+            </div>
+
+            <div className="recent">
+              <p className="recent-title">Recent</p>
+              <div className="recent-entry">
+                <img src={assets.message_icon} alt="" />
+                <p>What is NextJS ...</p>
+              </div>
+            </div>
         </div>
         <div className="bottom"></div>
     </div>
